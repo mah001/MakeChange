325f09fd0d22d00c440f8fc50bfbfdfa03fc86a4
mah001 <mahmoodim@vcu.edu>
2017-09-24 16:33:58 -0700

    Finally figured out how to calculate

--- MakeC/MakeC.cpp
+++ MakeC/MakeC.cpp
@@ -8,25 +8,25 @@ using namespace std;
 
 int main()
 {
-	double nickles;
-	double dimes;
-	double quarters;
-	double dollars;
-	double cost;
-	double amount;
 	double changedue;
+	double amount;
+	double cost;
+	int x;
+	int dollars;
+	int quarters;
+	int dimes;
+	int nickels;
+	int pennies;
 
-	cout << "cost" << cost << endl;
+	cout << "Please enter the Cost of the item: " << endl;
 	cin >> cost;
-	cout << "amount" << amount << endl;
+	cout << "Please enter the amount paid: " << endl;
 	cin >> amount;
-
-
-
 
 	while (amount<cost)
 	{
-		cout << "that is not enough you need " << (cost = cost - amount) << "more" << endl;
+
+		cout << "that is not enough you need " << (cost = cost - amount) << " more" << endl;
 		cin >> amount;
 		cout << "Please enter additional amount : " << (cost = cost - amount) << endl;
 		cin >> amount;
@@ -37,27 +37,32 @@ int main()
 	}
 	while (amount>cost)
 	{
+		changedue = amount - cost;
 		cout << "Item Price: " << cost << endl;
 		cout << "amount Paid: " << amount << endl;
-		cout << "change due: " << (amount = amount - cost) << endl;
-		if (amount >= 1)
+		cout << "change due: " << changedue << endl;
+		if ((amount >= 1) && (amount >= 0))
 		{
-			cout << "Dollars " << (dollars = amount - dollars / 100) << endl;
-		}
-		else if ((amount<1) && (amount>0.25))
-		{
-			cout << "Quarters " << (quarters = amount - quarters / 25) << endl;
+			x = changedue * 100;
+			dollars = x / 100;
+			x = x % 100;
+			cout << "Dollars: " << dollars << endl;
 
-		}
-		else if ((amount<0.25) && (amount>0.10))
-		{
-			cout << "nickles " << (nickles = amount - nickles / 10) << endl;
+			quarters = x / 25;
+			x = x % 25;
+			cout << "Quarters: " << quarters << endl;
 
-		}
-		else if (amount<0.10)
-		{
-			cout << "dimes " << (dimes = amount - dimes / 10) << endl;
+			dimes = x / 10;
+			x = x % 10;
+			cout << "Dimes: " << dimes << endl;
 
+			nickels = x / 5;
+			x = x % 5;
+			cout << "Nickles: " << nickels << endl;
+
+			pennies = x / 1;
+			x = pennies;
+			cout << "Pennies: " << pennies << endl;
 		}
 		else
 		{
@@ -70,9 +75,7 @@ int main()
 			cin >> cost;
 			break;
 		}
-
-
 	}
-
 	return 0;
 }
+
